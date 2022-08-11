30004a98a61b8fe8e13e99c85d5d752c8f8a3459
Godyl <110470042+GodylTech@users.noreply.github.com>
2022-08-11 11:42:27 +0200

    Update main.py
    Path of logo.png has been changed.

--- password_manager/main.py
+++ password_manager/main.py
@@ -51,7 +51,7 @@ def save_data():
 
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="pics/logo.png")
+logo_img = PhotoImage(file="data/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
